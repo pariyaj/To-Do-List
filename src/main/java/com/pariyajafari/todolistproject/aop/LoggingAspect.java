fea16e82342bfb5721eda9db8255f8b5a41e5853
pariyaj <pariyajafari95@gmail.com>
2024-11-12 23:39:08 +0330

    Add logging for throwable methods

--- src/main/java/com/pariyajafari/todolistproject/aop/LoggingAspect.java
+++ src/main/java/com/pariyajafari/todolistproject/aop/LoggingAspect.java
@@ -1,10 +1,7 @@
 package com.pariyajafari.todolistproject.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,13 +10,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Pointcut("execution(* com.pariyajafari.todolistproject.service.TaskService.getAllTasks())(..))")
     public void getTaskPointcut(){ }
 
     @After("getTaskPointcut()")
     public void logMethodExecution(JoinPoint joinPoint){
-        logger.info("Method " + joinPoint.getSignature().getName() + " executed successfully" );
+        LOGGER.info("Method " + joinPoint.getSignature().getName() + " executed successfully" );
+    }
+
+    @Pointcut("execution(* com.pariyajafari.todolistproject.service.TaskService.getTaskById())(..))")
+    public void getTaskByIdPointcut(){ }
+
+    @AfterThrowing("getTaskByIdPointcut()()")
+    public void logMethodExecutionCrashed(JoinPoint joinPoint){
+        LOGGER.info("Method " + joinPoint.getSignature().getName() + " threw an exception" );
     }
 }
