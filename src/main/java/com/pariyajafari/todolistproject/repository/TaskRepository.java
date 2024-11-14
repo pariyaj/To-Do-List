86510b7b8fac2a7102620d586e9411d9a908e848
pariyaj <pariyajafari95@gmail.com>
2024-11-14 15:50:02 +0330

    Add native queries to sort tasks

--- src/main/java/com/pariyajafari/todolistproject/repository/TaskRepository.java
+++ src/main/java/com/pariyajafari/todolistproject/repository/TaskRepository.java
@@ -2,8 +2,18 @@ package com.pariyajafari.todolistproject.repository;
 
 import com.pariyajafari.todolistproject.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query(value = "SELECT t FROM Task t order by deadLine" , nativeQuery = true)
+    List<Task> findAllSortedByDeadline();
+
+    @Query(value = "SELECT t FROM Task t order by name" , nativeQuery = true)
+    List<Task> findAllSortedByName();
+
 }
