ab9d512c43609afc3c2f85645138cb89d751088a
pariyaj <pariyajafari95@gmail.com>
2024-11-23 02:38:33 +0330

    Add logging controller class

--- src/main/java/com/pariyajafari/todolistproject/controller/Controller.java
+++ src/main/java/com/pariyajafari/todolistproject/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.pariyajafari.todolistproject;
+package com.pariyajafari.todolistproject.controller;
 
 import com.pariyajafari.todolistproject.model.Task;
 import com.pariyajafari.todolistproject.service.TaskService;
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
